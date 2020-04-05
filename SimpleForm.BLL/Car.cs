6c1e822cd15e0e994c79864dc3374d82897316a8
Kamil Rozanski <motomc1@gmail.com>
2020-04-05 11:56:35 +0100

    Car Class

---
+++ SimpleForm.BLL/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleForm.BLL
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string carBrand)
+        {
+            CarBrand = carBrand;
+            SetSteringWheel();
+
+        }
+        //var car = new Car("aaa");
+
+        public string CarBrand { get; set; }
+        public String CarColor { get; set; }
+        public int NumberOfSeats { get; set; }
+        public SteringWheel SteringWheel { get; set; }
+
+
+        public void SetColor(string color)
+        {
+            CarColor = color;
+        }
+
+        public void SetNumberOfSeatsr(int numberOfSeats)
+        {
+            NumberOfSeats = numberOfSeats;
+        }
+
+        public void SetSteringWheel()
+        {
+            if (CarBrand == "Fiat")
+            {
+                SteringWheel.IsLeather = false;
+            }
+            else if (CarBrand == "Toyota")
+            {
+                SteringWheel.IsLeather = true;
+            }
+        }
+
+    }
+
+    public class SteringWheel
+    {
+        public string Shape { get; set; }
+        public bool IsLeather { get; set; }
+    }
+
+
+}
